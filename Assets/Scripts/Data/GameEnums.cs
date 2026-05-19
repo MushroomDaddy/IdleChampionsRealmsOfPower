bfd52e50b3c430a04eb6e6ca14c11e1ebe9f8eda
root <root@srv1675651.hstgr.cloud>
2026-05-19 13:45:12 +0000

    Fix: Add missing GameEnums.cs (StatType, GearSlot, HeroClass, etc.)

---
+++ Assets/Scripts/Data/GameEnums.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public enum HeroClass {
+    Knight,
+    Ranger,
+    Mage
+}
+
+public enum GearSlot {
+    Weapon,
+    Helmet,
+    Chest,
+    Gloves,
+    Boots,
+    Ring
+}
+
+public enum GearRarity {
+    Common,     // Gray
+    Uncommon,   // Green
+    Rare,       // Blue
+    Epic,       // Purple
+    Legendary,   // Orange
+    Mythic      // Red
+}
+
+public enum PetRarity {
+    OneStar,
+    TwoStar,
+    ThreeStar,
+    FourStar,
+    FiveStar
+}
+
+public enum PetType {
+    Physical,
+    Magic,
+    Defense,
+    Speed,
+    Support
+}
+
+public enum StatType {
+    HP,
+    MaxHP,
+    Attack,
+    Magic,
+    Defense,
+    MagicDefense,
+    Speed,
+    CritRate,
+    CritDamage,
+    Dodge,
+    Accuracy,
+    Block,
+    Lifesteal,
+    Thorns,
+    Tenacity
+}
+
+public enum BattleSpeed {
+    Normal = 1,
+    Fast = 2,
+    UltraFast = 4
+}
+
+public enum SkillType {
+    Active,
+    Passive,
+    Ultimate
+}
+
+public enum ElementType {
+    None,
+    Fire,
+    Water,
+    Earth,
+    Air,
+    Light,
+    Dark
+}
